ea74491ee40b6f17aeb779de94f64945b572e0ea
unpete <info@oknosoft.ru>
2020-05-27 13:19:08 +0500

    initial commit

---
+++ src/Common/CloseBtn.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Tip from './Tip';
+
+export default function CloseBtn(props) {
+  return <Tip title="Закрыть форму">
+    <IconButton onClick={props.handleClose}><CloseIcon/></IconButton>
+  </Tip>;
+}
+
+CloseBtn.propTypes = {
+  handleClose: PropTypes.func.isRequired,
+};
