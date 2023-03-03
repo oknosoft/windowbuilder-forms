80f8036da5dc3758b9ad5a126d1c7d01253d8b2c
unpete <info@oknosoft.ru>
2023-03-03 22:03:28 +0500

    дискретный ряд в LinkedProp

--- src/Common/LinkedProp.js
+++ src/Common/LinkedProp.js
@@ -10,47 +10,72 @@ export default function LinkedProp({_obj, _fld='value', param, fields, cnstr, in
   if(!param) {
     param = _obj.param;
   }
-  const lnk_props = {obj: _obj};
-  if(cnstr !== undefined) {
-    lnk_props.grid = {selection: {cnstr, inset}};
-  }
-  const links = param.params_links(lnk_props);
+
   // вычисляемые скрываем всегда
   let hide = !param.show_calculated && param.is_calculated;
-  // если для параметра есть связи - сокрытие по связям
-  if(!hide){
-    if(links.length) {
-      hide = links.some((link) => link.hide);
-    }
-    else {
-      hide = _obj.hide;
-    }
-  }
+
+  // признак короткого списка
+  const {types} = param.type;
+  let oselect = types.length === 1 && ['cat.property_values', 'cat.characteristics'].includes(types[0]);
 
   const _meta = $p.utils._clone(fields[_fld]);
   _meta.synonym = param.caption || param.name;
 
-  const {types} = param.type;
-  let oselect = types.length === 1 && ['cat.property_values', 'cat.characteristics'].includes(types[0]);
-
-  // проверим вхождение значения в доступные и при необходимости изменим
-  if (links.length) {
-    const values = [];
-    param.linked_values(links, null, values);
-    if(values.length) {
-      if(values.length < 50) {
+  // учтём дискретный ряд - он приоритетнее связей параметров
+  const drow = inset?.product_params?.find({param});
+  if(drow) {
+    if(!hide){
+      hide = hide.hide || _obj.hide;
+    }
+    if(drow?.list) {
+      try{
+        _meta.list = JSON.parse(drow.list);
         oselect = true;
       }
-      if(!_meta.choice_params) {
-        _meta.choice_params = [];
+      catch (e) {
+        delete _meta.list;
       }
-      // дополняем отбор
-      _meta.choice_params.push({
-        name: 'ref',
-        path: {in: values.map((v) => v.value)}
-      });
+    }
+    else {
+      delete _meta.list;
     }
   }
+  else {
+    // если нет умолчаний во вставке, используем связи
+    const lnk_props = {obj: _obj};
+    if(cnstr !== undefined) {
+      lnk_props.grid = {selection: {cnstr, inset}};
+    }
+    const links = param.params_links(lnk_props);
+    // если для параметра есть связи - сокрытие по связям
+    if(!hide){
+      if(links.length) {
+        hide = links.some((link) => link.hide);
+      }
+      else {
+        hide = _obj.hide;
+      }
+    }
+    // дополним метаданные отбором
+    if (links.length) {
+      const values = [];
+      param.linked_values(links, null, values);
+      if(values.length) {
+        if(values.length < 50) {
+          oselect = true;
+        }
+        if(!_meta.choice_params) {
+          _meta.choice_params = [];
+        }
+        // дополняем отбор
+        _meta.choice_params.push({
+          name: 'ref',
+          path: {in: values.map((v) => v.value)}
+        });
+      }
+    }
+  }
+
   if (typeof _obj.hide === 'boolean' && _obj.hide !== hide) {
     _obj.hide = hide;
   }
