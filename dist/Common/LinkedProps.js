421c648a0ac026b4d858eff946641545042efae9
unpete <info@oknosoft.ru>
2024-08-28 09:42:45 +0300

    группировка в LinkedProps

--- dist/Common/LinkedProps.js
+++ dist/Common/LinkedProps.js
@@ -9,35 +9,102 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PropField from 'metadata-react/DataField/PropField';
-export default function LinkedProps({
+import CssField from './CssField';
+import FieldSet from './FieldSet';
+class FakePrmRow {
+  constructor(raw) {
+    this._raw = raw; //{ts, cnstr, param, inset, value};
+    this._rows = [];
+  }
+  add(row) {
+    if (!this._rows.includes(row)) {
+      this._rows.push(row);
+    }
+  }
+  isDiff() {
+    const {
+      _raw: {
+        cnstr,
+        value
+      },
+      _rows
+    } = this;
+    const row0 = _rows.find(v => v.cnstr === 0);
+    return cnstr && row0 && row0.value != value;
+  }
+  get _owner() {
+    return this._raw.ts;
+  }
+  get _row() {
+    return this._rows[0];
+  }
+  get row() {
+    return this._raw.cnstr || this._raw.inset?.valueOf() || 0;
+  }
+  get hide() {
+    return false;
+  }
+  set hide(v) {}
+  get cnstr() {
+    return this._raw.cnstr;
+  }
+  get param() {
+    return this._raw.param;
+  }
+  get sorting_field() {
+    return this._raw.param.sorting_field;
+  }
+  get value() {
+    return this._raw.value;
+  }
+  set value(v) {
+    const {
+      _raw: {
+        cnstr,
+        param,
+        inset,
+        ts
+      },
+      _rows
+    } = this;
+    const row0 = _rows.find(v => v.cnstr === 0);
+    const value = this._raw.param.fetch_type(v);
+    if (this._raw.cnstr) {
+      let rowc = _rows.find(v => v.cnstr === cnstr);
+      if (value == row0?.value) {
+        // если значение в слое равно значению в изделии
+        if (row0) {
+          if (rowc) {
+            _rows.splice(_rows.indexOf(rowc), 1);
+            ts.del(rowc);
+          }
+        }
+      } else {
+        if (!rowc) {
+          rowc = ts.add({
+            cnstr,
+            param,
+            inset
+          });
+          _rows.push(rowc);
+        }
+        rowc.value = value;
+      }
+    } else if (row0) {
+      row0.value = value;
+    }
+    this._raw.value = value;
+  }
+}
+function groupedRows({
   ts,
   cnstr,
   inset,
   layer,
-  project
+  sys
 }) {
-  const {
-    _owner
-  } = ts;
-  const {
-    fields
-  } = _owner._metadata(ts._name);
-  const res = [];
-  const grid = {
-    selection: {
-      cnstr,
-      inset
-    }
-  };
-  const notify = new Set();
-  const sys = layer ? layer.sys : _owner.sys;
-  const is_template = _owner?.calc_order?.obj_delivery_state?.is('Шаблон');
-  const {
-    utils,
-    job_prm: {
-      properties
-    }
-  } = $p;
+  const raw = new Map();
+  const aprm = layer?.furn_set?.used_params();
   ts.find_rows({
     cnstr,
     inset,
@@ -46,14 +113,101 @@ export default function LinkedProps({
     const {
       param
     } = prow;
-    if (param === properties.auto_align && (cnstr || !is_template)) {
-      return;
+    if (!raw.has(param)) {
+      raw.set(param, new FakePrmRow({
+        ts,
+        cnstr,
+        param,
+        inset,
+        value: prow.value
+      }));
+    }
+    raw.get(param).add(prow);
+  });
+  if (aprm) {
+    for (const param of aprm) {
+      const topRow = ts.find({
+        cnstr: 0,
+        param,
+        inset,
+        hide: false
+      });
+      if (topRow) {
+        if (!raw.has(param)) {
+          raw.set(param, new FakePrmRow({
+            ts,
+            cnstr,
+            param,
+            value: topRow.value
+          }));
+        }
+        raw.get(param)?.add(topRow);
+      }
+    }
+  }
+  const grouped = [];
+  const {
+    job_prm: {
+      lists: {
+        sys_prm_grp_names
+      }
+    },
+    utils
+  } = $p;
+  for (const [param, prow] of raw) {
+    let grp = 0;
+    if (sys_prm_grp_names?.length && sys) {
+      const row = sys.product_params.find({
+        param
+      }) || sys.furn_params.find({
+        param
+      });
+      if (row) {
+        grp = row.grouping;
+      }
+    }
+    if (!grouped[grp]) {
+      grouped[grp] = {
+        name: sys_prm_grp_names[grp - 1] || '',
+        rows: []
+      };
+    }
+    grouped[grp].rows.push(prow);
+  }
+  const sort = utils.sort('sorting_field');
+  for (const elm of grouped) {
+    elm?.rows?.sort?.(sort);
+  }
+  return grouped.filter(v => v?.rows);
+}
+function children({
+  rows,
+  layer,
+  project,
+  fields,
+  sys,
+  grid,
+  _owner
+}) {
+  const {
+    utils,
+    job_prm: {
+      properties
+    }
+  } = $p;
+  const res = [];
+  const notify = new Set();
+  const is_template = _owner?.calc_order?.obj_delivery_state?.is('Шаблон');
+  for (const prow of rows) {
+    const {
+      param
+    } = prow;
+    if (param === properties.auto_align && (grid.selection.cnstr || !is_template)) {
+      continue;
     }
     const _meta = utils._clone(fields.value);
-    _meta.synonym = param.name || param.caption;
-    if (param.mandatory) {
-      _meta.mandatory = true;
-    }
+    _meta.synonym = `${prow.isDiff() ? '* ' : ''}${param.name || param.caption}`;
+    _meta.type = param.type;
     const {
       types
     } = param.type;
@@ -68,6 +222,7 @@ export default function LinkedProps({
       key += sys.valueOf();
     }
     let hide = false;
+    const values = [];
     if (!(project || layer).params_links(stub)) {
       const links = param.params_links({
         grid,
@@ -90,7 +245,6 @@ export default function LinkedProps({
         links.forEach(link => {
           key += link.valueOf();
         });
-        const values = [];
         if (param.linked_values(links, prow, values)) {
           notify.add(prow);
         }
@@ -111,31 +265,111 @@ export default function LinkedProps({
         }
       }
     }
+    if (values.some(v => v.value.css)) {
+      stub.cssselect = values.map(v => v.value);
+    }
     if (prow.hide !== hide) {
       prow.hide = hide;
       notify.add(prow);
     }
     if (hide) {
-      return;
+      continue;
     }
-    res.push({
+    const elm = {
       key,
       prow,
       _meta,
       ctrl_type: stub.oselect ? 'oselect' : bool ? 'ch' : void 0,
-      sorting_field: param.sorting_field
-    });
-  });
-  return res.sort(utils.sort('sorting_field')).map(({
+      cssselect: stub.cssselect
+    };
+    if (param.inheritance === 5) {
+      elm.read_only = !_owner.obj_delivery_state?.is('Шаблон');
+    }
+    res.push(elm);
+  }
+  return res.map(({
     key,
     prow,
     _meta,
-    ctrl_type
-  }) => /*#__PURE__*/React.createElement(PropField, {
+    ctrl_type,
+    cssselect,
+    read_only
+  }) => cssselect ? /*#__PURE__*/React.createElement(PropField, {
     key: `prm-${utils.crc32(key)}`,
     _obj: prow,
     _fld: "value",
     _meta: _meta,
+    read_only: read_only,
+    options: cssselect,
+    Component: CssField
+  }) : /*#__PURE__*/React.createElement(PropField, {
+    key: `prm-${utils.crc32(key)}`,
+    _obj: prow,
+    _fld: "value",
+    _meta: _meta,
+    read_only: read_only,
     ctrl_type: ctrl_type
   }));
+}
+export default function LinkedProps({
+  ts,
+  cnstr,
+  inset,
+  layer,
+  project
+}) {
+  const {
+    _owner
+  } = ts;
+  const {
+    fields
+  } = _owner._metadata(ts._name);
+  const sys = layer ? layer.sys : _owner.sys;
+  const grid = {
+    selection: {
+      cnstr,
+      inset
+    }
+  };
+
+  // подготовим массив свойств
+  const grouped = groupedRows({
+    ts,
+    cnstr,
+    inset,
+    layer,
+    sys
+  });
+  if (!grouped.length) {
+    return null;
+  } else if (grouped.length === 1) {
+    return children({
+      rows: grouped[0].rows,
+      layer,
+      project,
+      fields,
+      sys,
+      grid,
+      _owner
+    });
+  } else {
+    return grouped.map(({
+      name,
+      rows
+    }, index) => {
+      const elements = children({
+        rows,
+        layer,
+        project,
+        fields,
+        sys,
+        grid,
+        _owner
+      });
+      return name ? /*#__PURE__*/React.createElement(FieldSet, {
+        key: `grp-${index}`,
+        title: name
+      }, elements) : elements;
+    });
+  }
 }
